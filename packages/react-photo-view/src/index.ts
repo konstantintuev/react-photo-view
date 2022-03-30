cad254b9adf1c22942339545f89bd80fc82c6260
Konstantin Tuev <kosiomt11@gmail.com>
2022-03-30 11:14:50 +0300

    Export `useInitial`, `useMethods`, `PhotoContext` as to allow custom PhotoView implementations

--- packages/react-photo-view/src/index.ts
+++ packages/react-photo-view/src/index.ts
@@ -1,5 +1,8 @@
 import PhotoProvider from './PhotoProvider';
 import PhotoView from './PhotoView';
 import PhotoSlider from './PhotoSlider';
+import useInitial from './hooks/useInitial';
+import useMethods from './hooks/useMethods';
+import PhotoContext from './photo-context';
 
-export { PhotoProvider, PhotoView, PhotoSlider };
+export { PhotoProvider, PhotoView, PhotoSlider, useInitial, useMethods, PhotoContext };
